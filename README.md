937ba74bc10086b282353a5c8c5855b650b93f11
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-05 16:38:26 -0600

    Complete user story 18 - add links to pet show pages on pets index page

--- README.md
+++ README.md
@@ -363,7 +363,7 @@ Then that link takes me to that Shelter's show page
 ```
 
 ```
-[ ] done
+[X] done
 
 User Story 18, Pet Links
 
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -96,6 +96,28 @@ RSpec.describe "when I visit the pets index page", type: :feature do
 
     expect(page).to_not have_content("Max")
   end
+
+  it "I can click on a pet to view its show page" do
+    shelter_1 = Shelter.create(name: "Pig Paradise",
+                              address: "123 Turing Ave",
+                              city: "Denver",
+                              state: "Colorado",
+                              zip: "80216")
+
+    pet_1 = Pet.create(name: "Max",
+                      image: "https://cdn.pixabay.com/photo/2016/01/22/12/23/pig-1155658_1280.jpg",
+                      age: 2,
+                      sex: "Female",
+                      description: "Find your next gal pal in Max the teacup pig.",
+                      adoption_status: "Adoptable",
+                      shelter_id: shelter_1.id)
+
+    visit '/pets'
+
+    click_on "#{pet_1.name}"
+
+    expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
 end
 
 
