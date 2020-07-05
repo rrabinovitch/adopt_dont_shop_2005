c286d4efce8821399d96d343d2c0b9b7444cb9ba
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-05 14:03:01 -0600

    Write test for user story 13 + add html id to 'Update Shelter' buttons on shelters index page

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -31,15 +31,41 @@ RSpec.describe "when I visit the pets index page", type: :feature do
     expect(page).to have_content(pet_1.age)
     expect(page).to have_content(pet_1.sex)
     expect(page).to have_content(Shelter.find(pet_1.shelter_id).name)
-    # expect(page).to have_content(Shelter.where("id = #{pet_1.shelter_id}")[0].name)
 
     expect(page).to have_content(pet_2.name)
     expect(page).to have_xpath("//img[@src='#{pet_2.image}']")
     expect(page).to have_content(pet_2.age)
     expect(page).to have_content(pet_2.sex)
     expect(page).to have_content(Shelter.find(pet_2.shelter_id).name)
-    # expect(page).to have_content(Shelter.where("id = #{pet_2.shelter_id}")[0].name)
   end
+
+  # it "I can select a specific pet to edit its info" do
+  #   shelter_1 = Shelter.create(name: "Pig Paradise",
+  #                             address: "123 Turing Ave",
+  #                             city: "Denver",
+  #                             state: "Colorado",
+  #                             zip: "80216")
+  #
+  #   pet_1 = Pet.create(name: "Max",
+  #                     image: "https://cdn.pixabay.com/photo/2016/01/22/12/23/pig-1155658_1280.jpg",
+  #                     age: 2,
+  #                     sex: "Female",
+  #                     description: "Find your next gal pal in Max the teacup pig.",
+  #                     adoption_status: "Adoptable",
+  #                     shelter_id: shelter_1.id)
+  #
+  #   pet_2 = Pet.create(name: "Deb",
+  #                     image: "https://petpigs.com/wp-content/uploads/2011/04/cute-pig-NAPPA.jpg",
+  #                     age: 26,
+  #                     sex: "Female",
+  #                     description: "She's a gift.",
+  #                     adoption_status: "Adoptable",
+  #                     shelter_id: shelter_1.id)
+  #
+  #   visit '/pets'
+  #
+  #   click_on
+  # end
 end
 
 
--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -39,4 +39,30 @@ RSpec.describe "when I visit the shelters index page", type: :feature do
     expect(current_path).to eq('/shelters')
     expect(page).to have_content("Pig Paradise")
   end
+
+  it "I can select a specific shelter to edit its info" do
+    shelter_1 = Shelter.create(name: "Pig Paradise",
+                              address: "123 Turing Ave",
+                              city: "Denver",
+                              state: "Colorado",
+                              zip: "80216")
+    shelter_2 = Shelter.create(name: "The Oinkery",
+                              address: "543 Main St",
+                              city: "Aurora",
+                              state: "Colorado",
+                              zip: "80255")
+
+    visit '/shelters'
+
+    expect(page).to_not have_content("Pig Paradise LLC")
+
+    find("##{shelter_1.name.gsub(" ", "")}Edit").click
+
+    fill_in :name, with: "Pig Paradise LLC"
+
+    click_on 'Update'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(page).to have_content("Pig Paradise LLC")
+  end
 end
