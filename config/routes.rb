83ec91be7688fc4f9bfa62667f7fa12c8bf92751
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-05 16:06:24 -0600

    Revert root back to 'home#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'shelters#index'
+  root 'home#index'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
